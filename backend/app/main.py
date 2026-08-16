d445115a19b5a468223c84db4a7b9c579abc1814
Аттор Азизов <attorazizov@MacBook-Air-Attor.local>
2026-08-17 01:47:38 +0300

    Final

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database import Base, engine
+from app import models
 from app.routers import auth, users, applications
 
 
-app = FastAPI(
-    title="JobFlow API"
-)
+app = FastAPI(title="JobFlow API")
+
+Base.metadata.create_all(bind=engine)
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://jobflow-1-iyuj.onrender.com"],
+    allow_origin_regex=r"https://.*\.onrender\.com",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
